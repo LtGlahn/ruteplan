d5ead38306cf880783b723a15c596177b43912fa
LtGlahn <jensen72@gmail.com>
2017-08-25 15:32:42 +0200

    parsing results from ruteplan => list of geojson features

--- ruteplan.py
+++ ruteplan.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """Python-functions for routing directions from the Norwegian Public Road Administration 
 routing API https://data.norge.no/data/statens-vegvesen/api-ruteplantjeneste-bil
 
@@ -19,6 +21,7 @@ content:
 import requests
 import json
 import copy
+import geojson
 
 def lescredfil( credfile = 'credentials.json', server='ruteplan' ):
   
@@ -50,9 +53,64 @@ def lescredfil( credfile = 'credentials.json', server='ruteplan' ):
     return credentials
     
 
+def parseruteplan( responseobj, egenskaper={}  ): 
+    """Tar responsobjekt fra ruteplantjenesten, omsetter til en 
+    liste med Geojson-features. Disse kan puttes inn i en featureCollection
+    eller bearbeides med annet vis. 
+    
+    Hvis du har andre (meta)data som du vil knytte til ruteforslaget
+    kan dette tas med via parameteren egenskaper={'key':'val'} 
+    """
+    
+    # Feilsituasjoner? 
+    if not responseobj.ok: 
+        message= ' '.join( [ 'Invalid response from ruteplan:', 
+                            str(r.status_code), r.reason, r.url ])
+        raise ValueError( message )
+
+    data = responseobj.json()
+    if 'messages' in data.keys():
+        message = str( data['messages']) + ' ' + r.url
+        raise ValueError( message)
+
+    featurelist = []
+    for ii, rute in enumerate( data['routes']['features']): 
+        
+        # Fjerner de egenskapene vi ikke vil ha: 
+        attributes = rute['attributes']
+        extra_attributes = attributes.pop( 'attributes')
+        attributes.pop( 'ObjectID')
+        attributes.pop('Shape_Length')
+        
+        # Lager attributtliste av de egenskapene som er igjen: 
+        egen2 = copy.deepcopy( egenskaper)
+        for k in attributes.keys():
+            egen2[k] = attributes[k]
+            
+        # Parser listen med ekstra attributter
+        for val in extra_attributes:
+            egen2[val['key']] = val['value']
+            
+        # Henter litt snacks fra directions-elementet: 
+        egen2['routeName'] = data['directions'][ii]['routeName']
+        
+        # Legger på info om beste - nestbeste osv
+        egen2['rutealternativNr'] = ii
+        
+        # Behandler geometri
+        mygeom =  geojson.LineString( rute['geometry']['paths'][0])
+        
+        # Lager geojson-objekt
+        mygeojs = geojson.Feature( geometry=mygeom, properties=egen2)
+
+
+        featurelist.append( mygeojs)
+          
+    return featurelist
+
 def anropruteplan( ruteplanparams={ 'format' :  'json', 'geometryformat' : 'isoz' }, 
-                  server='ruteplan', coordinates = [(274327.47280083, 6656639.067924), 
-                   (275727.78289313,6656983.0269452)]): 
+                  server='ruteplan', coordinates = 
+    [ (269756.5,7038421.3), (269682.4,7039315.6)]): 
     """Fetch data from the NVDB roting API 
     
     ruteplanparams = dict with parameters for the routing applications. 
@@ -67,9 +125,13 @@ def anropruteplan( ruteplanparams={ 'format' :  'json', 'geometryformat' : 'isoz
     Returns a request response object
     """
 
-	# Henter info om server, pålogging etc
+	# Henter info om server, brukernavn etc
     credentials = lescredfil( credfile='credentials.json', server=server)
-        
+    
+    # Har vi angitt proxy? 
+    proxies = {}
+    if 'proxies' in credentials.keys(): 
+        proxies = credentials['proxies']
     
     # Har vi eksplisitt angitt "stops" i ruteplanparametrene? 
     # Hvis ikke bygger vi det ut fra koordinatene
@@ -87,9 +149,11 @@ def anropruteplan( ruteplanparams={ 'format' :  'json', 'geometryformat' : 'isoz
         
     if credentials['auth']:
         r = requests.get( credentials['url'], auth=credentials['auth'], 
-                         params=ruteplanparams)
+                         params=ruteplanparams, proxies=proxies)
         
     else: 
-        r = requests.get( credentials['url'], params=ruteplanparams)
+        r = requests.get( credentials['url'], params=ruteplanparams, 
+                         proxies=proxies)
         
     return r
+
